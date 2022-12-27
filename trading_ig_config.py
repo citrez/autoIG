71a506280f2e5cc34f77575eccad9dde62e8b742
Ezra Citron <ezracitron@gmail.com>
2022-12-27 00:20:05 +0000

    remove

--- trading_ig_config.py
+++
@@ -1,9 +0,0 @@
-# !/usr/bin/env python
-# -*- coding:utf-8 -*-
-
-class config(object):
-    username = "citrez"
-    password = "-V$2AqT3VWunJ#r"
-    api_key = "a5449a3130022c374536c053e334b7a61fb59b33"
-    acc_type = "DEMO"  # LIVE / DEMO
-    acc_number = "CURTV"
