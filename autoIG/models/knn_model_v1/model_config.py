9cd7d0f8e484b102ef2346e879576dea4920b5ce
Ezra Citron <ezracitron@gmail.com>
2023-01-09 23:23:29 +0000

    adding threshold to model config

--- autoIG/models/knn_model_v1/model_config.py
+++ autoIG/models/knn_model_v1/model_config.py
@@ -3,3 +3,4 @@ from autoIG.instruments import Epics,Tickers
 source = Source.yahoo_finance.value
 epic = Epics.US_CRUDE_OIL.value  # Could get both of these based on US_CRUDE_OIL and source
 ticker = Tickers.US_CRUDE_OIL.value
+threshold = 1.01 # This is the threshold that we should use in prod? But really this should be a seperate piece of analysis?
