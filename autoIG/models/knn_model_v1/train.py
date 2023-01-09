7c57c57ba1d700f97d5506272994470fd70ead85
Ezra Citron <ezracitron@gmail.com>
2023-01-09 23:25:45 +0000

    improve and include knn in mlflow logging

--- autoIG/models/knn_model_v1/train.py
+++ autoIG/models/knn_model_v1/train.py
@@ -6,11 +6,13 @@ import mlflow
 import mlflow.sklearn
 import mlflow.models
 import pandas as pd
+from sklearn.utils import estimator_html_repr
 from sklearn import set_config
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import FunctionTransformer
 from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
 
 from autoIG.modelling import create_future_bid_Open, generate_target
 from autoIG.modelling import (
@@ -49,11 +51,11 @@ def get_data(reload_data=False):
     if not reload_data:
         if source == "IG":
             model_data = pd.read_pickle(
-                Path(__file__).parent / "model_data/model_data_ig.pkl"
+                Path(__file__).parent / "model_data" / "model_data_ig.pkl"
             )
         if source == "YF":
             model_data = pd.read_pickle(
-                Path(__file__).parent / "model_data/model_data_yf.pkl"
+                Path(__file__).parent / "model_data" / "model_data_yf.pkl"
             )
 
     if reload_data:
@@ -144,14 +146,14 @@ pl.fit(X_train, y_train)
 if MLFLOW_RUN:
     with mlflow.start_run(
         # Uses experiment set in mlflow.set_experiment
-        # run_id= autolog_run.info.run_id
-        description="This is a description of the model run"
+        run_id=mlflow.last_active_run().info.run_id,
+        description="This is a description of the model run",
     ) as run:
         # Log model
         model_uri = f"runs:/{run.info.run_id}/{MODEL_NAME}"
         mlflow.sklearn.log_model(
             sk_model=pl,
-            artifact_path="sklearn-model",  # in the run, model artifacts are stored in artifacts/artifact_path
+            artifact_path="sklearn-model",  # In the run, model artifacts are stored in artifacts/artifact_path
             registered_model_name=MODEL_NAME,
             input_example=X_train.iloc[0:3, :],
             signature=mlflow.models.infer_signature(
@@ -164,8 +166,8 @@ if MLFLOW_RUN:
         ax1.set_title("train")
         ax2.scatter(y_test, pl.predict(X_test))
         ax2.set_title("test")
-        plt.xlabel("actual")
-        plt.ylabel("prediction")
+        plt.xlabel("y_true")
+        plt.ylabel("y_preds")
         plt.suptitle("training_and_testing_predictions_scatter")
 
         mlflow.log_figure(fig, "training_and_testing_predictions_scatter.png")
@@ -176,12 +178,13 @@ if MLFLOW_RUN:
             [y_train, pl.predict(X_train)],
             bins=bins,
             alpha=0.5,
+            range=(0.999, 1.001),
             label=["actual", "predictions"],
         )
         plt.legend(loc="upper right")
-        plt.suptitle("predicted_histogram")
+        plt.suptitle("training y_true y_preds")
 
-        mlflow.log_figure(fig, "predicted_histogram.png")
+        mlflow.log_figure(fig, "training_y_true_y_preds.png")
 
         fig, ax = plt.subplots(2)
         ax[0].scatter(y_train, (y_train - pl.predict(X_train)))
@@ -200,18 +203,32 @@ if MLFLOW_RUN:
         mlflow.log_param("source", source)
         mlflow.log_param("epic", epic)
         mlflow.log_param("ticker", ticker)
-        mlflow.log_dict(reload_data_config, "historical_prices_config.json")
+        mlflow.log_dict(
+            reload_data_config,
+            "historical_prices_config.json",
+        )
 
         mlflow.log_metric(
             "testing_mae",
             mean_absolute_error(y_pred=pl.predict(X_test), y_true=y_test),
-        )  
+        )
         mlflow.log_metric(
             "training_mae",
-            mean_absolute_error(y_pred=pl.predict(X_test), y_true=y_train),
+            mean_absolute_error(y_pred=pl.predict(X_train), y_true=y_train),
         )
+        CURRENT_DIR = Path(__file__).parent
+        with open(CURRENT_DIR / "pl.html", "w") as f:
+            f.write(estimator_html_repr(pl))
+        mlflow.log_artifact(local_path=CURRENT_DIR / "pl.html", artifact_path="docs")
 
         ## KNN specific metrics
-        
+        mlflow.log_metric('testing_neigh_dist_n_neighbors_1',pl.named_steps.predictor.kneighbors(pl[:-1].transform(X_test),n_neighbors=1)[0].sum())
+        mlflow.log_metric('training_neigh_dist_n_neighbors_1',pl.named_steps.predictor.kneighbors(pl[:-1].transform(X_train),n_neighbors=1)[0].sum())
+
+        mlflow.log_metric('testing_neigh_dist_n_neighbors_5',pl.named_steps.predictor.kneighbors(pl[:-1].transform(X_test),n_neighbors=5)[0].sum())
+        mlflow.log_metric('training_neigh_dist_n_neighbors_5',pl.named_steps.predictor.kneighbors(pl[:-1].transform(X_train),n_neighbors=5)[0].sum())
+
+        mlflow.log_metric('testing_neigh_dist_n_neighbors_10',pl.named_steps.predictor.kneighbors(pl[:-1].transform(X_test),n_neighbors=10)[0].sum())
+        mlflow.log_metric('training_neigh_dist_n_neighbors_10',pl.named_steps.predictor.kneighbors(pl[:-1].transform(X_train),n_neighbors=10)[0].sum())
 
     print(f"Logged data and model in run {run.info.run_id}")
